0677ab2e4d92574da70c803770570fc05beaf6dd
Dmitry Khenkin <dmitry_khenkin@mail.ru>
2012-12-29 17:26:10 +0200

    .

--- current/test.sh
+++ current/test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+if [ $1_ == _ ]; then
+  echo Test regular expression.
+  echo Test expression from config file \"`dirname $0`/modules/cfg\" with logfile \"/var/log/cfg\"
+  echo Usage: `basename $0 ` cfg
+  echo
+  exit 1
+fi
+
 cat /var/log/$1 | ./filter-test $1
